fb5b287308363bf8fb5cd9e6be32f5d03870146f
jean carlos <jeancarlos80s@gmail.com>
2025-08-02 10:26:02 -0300

    add files to version

---
+++ src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
+
+@Component({
+  selector: 'front-navbar',
+  standalone: true,
+  imports: [
+    RouterLink, RouterLinkActive
+  ],
+  templateUrl: './front-navbar.component.html',
+  styleUrl: './front-navbar.component.css'
+})
+export class FrontNavbarComponent {
+
+}
---
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -0,0 +1,22 @@
+import {Component, inject} from '@angular/core';
+import {ProductCardComponent} from "@store-front/components/product-card/product-card.component";
+import {ProductService} from "@products/services/products.service";
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [
+    ProductCardComponent
+  ],
+  templateUrl: './home-page.component.html'
+})
+export class HomePageComponent {
+  productsService = inject(ProductService);
+
+  // productsResource = rxResource({
+  //   request: () => ({}),
+  //   loader: ({ request }) => {
+  //     return this.productsService.getProducts();
+  //   }
+  // })
+}
